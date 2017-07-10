70114f9ae4b8e10f006c02e4e804bbda0bc3066e
Armand Leclercq <armand@leclercq.site>
2017-07-10 18:14:08 +0000

    Import sqldiff.cc to initiate library building

---
+++ src/sqldiff.cc
@@ -0,0 +1,12 @@
+/*!
+ * \author: Armand Leclercq
+ * \file: sqldiff.cc
+ * \date: Mon 10 Jul 2017 06:12:22 PM UTC
+ */
+#include <iostream>
+
+void
+diff(const std::istream& /* a */ , const std::istream& /* b */ )
+{
+  std::cout << "Diff" << std::endl;
+}
