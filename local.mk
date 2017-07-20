10fd499225702af7cc0e0dc544a4b34129c4c3aa
Armand Leclercq <armand@leclercq.site>
2017-07-20 06:23:14 +0000

    Remove src/local.mk as test will need our include files

--- local.mk
+++ local.mk
@@ -1,2 +1,7 @@
-include %reldir%/src/local.mk
+lib_LTLIBRARIES += libsqldiff.la
+sqldiffdir = ${includedir}/sqldiff
+sqldiff_HEADERS = %reldir%/include/sqldiff.hh
+libsqldiff_la_SOURCES = %reldir%/src/sqldiff.cc
+libsqldiff_la_LDFLAGS = -version-info 0:0:0
+
 include %reldir%/test/local.mk
--- src/local.mk
+++
@@ -1,3 +0,0 @@
-lib_LTLIBRARIES += libsqldiff.la
-libsqldiff_la_SOURCES = %reldir%/sqldiff.cc
-libsqldiff_la_LDFLAGS = -version-info 0:0:0
