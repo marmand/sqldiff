b563b0998332008431fa94f83a08feb8aa92bedd
Armand Leclercq <armand@leclercq.site>
2017-07-20 06:26:50 +0000

    Use sqldiff library in tests

--- test/local.mk
+++ test/local.mk
@@ -1,6 +1,8 @@
 check_PROGRAMS = hello
-hello_SOURCES = %reldir%/hello.cc
-hello_LDFLAGS = ${GTEST_LIBS}
-hello_CXXFLAGS = ${PTHREAD_CFLAGS}
+hello_SOURCES =  \
+%reldir%/hello.cc
+
+hello_LDFLAGS = libsqldiff.la ${GTEST_LIBS}
+hello_CXXFLAGS = -I${srcdir}/include ${PTHREAD_CFLAGS}
 
 TESTS = $(check_PROGRAMS)
