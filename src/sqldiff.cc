b24112fcdb7c6b2c046c7f8d811666e79725b278
Armand Leclercq <armand@leclercq.site>
2017-07-20 06:25:10 +0000

    Create more target-like source file

--- src/sqldiff.cc
+++ src/sqldiff.cc
@@ -3,10 +3,7 @@
  * \file: sqldiff.cc
  * \date: Mon 10 Jul 2017 06:12:22 PM UTC
  */
-#include <iostream>
-
-void
-diff(const std::istream& /* a */ , const std::istream& /* b */ )
+namespace sqldiff
 {
-  std::cout << "Diff" << std::endl;
-}
+
+} /* namespace sqldiff */
