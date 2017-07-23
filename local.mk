9989d071e4ac1c999636efd2e1026062034468da
Armand Leclercq <armand@leclercq.site>
2017-07-23 16:21:54 +0000

    Make sure to load all sources

--- local.mk
+++ local.mk
@@ -1,7 +1,14 @@
 lib_LTLIBRARIES += libsqldiff.la
 sqldiffdir = ${includedir}/sqldiff
 sqldiff_HEADERS = %reldir%/include/sqldiff.hh
-libsqldiff_la_SOURCES = %reldir%/src/sqldiff.cc
+libsqldiff_la_SOURCES =         \
+%reldir%/src/column.hh          \
+%reldir%/src/sql.hh             \
+%reldir%/src/sql_grammar.hh     \
+%reldir%/src/sqldiff.cc         \
+%reldir%/src/statement.hh       \
+%reldir%/src/table.hh
+
 libsqldiff_la_LDFLAGS = -version-info 0:0:0
 libsqldiff_la_CXXFLAGS = -I${top_srcdir}/include/
 
