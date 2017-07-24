74b523c69e55c3725b07da5946e3c0dbc6052871
Armand Leclercq <armand@leclercq.site>
2017-07-24 18:16:52 +0000

    Correct indexes

--- src/diff.cc
+++ src/diff.cc
@@ -8,67 +8,57 @@
 #include <algorithm>
 #include <vector>
 
-#include <iostream>
-
 namespace sqldiff
 {
   size_t
   distance(const std::string& lhs
            , const std::string& rhs)
   {
-    std::vector<size_t> distances(lhs.size() * rhs.size(), 0);
-
-    // Personnal addition to handle special case
-    if (!lhs.size() || !rhs.size())
-    {
-      if (!lhs.size())
-        return rhs.size();
-      else // !rhs.size()
-        return lhs.size();
-    }
+    std::vector<size_t> distances((1 + lhs.size()) * (1 + rhs.size()), 0);
+    size_t line = lhs.size() + 1;
 
     // i goes through lhs
     // j goes through rhs
-
-    for (size_t i = 0; i < lhs.size(); ++i)
+    for (size_t i = 0; i <= lhs.size(); ++i)
       distances[i + 0] = i;
-    for (size_t j = 0; j < rhs.size(); ++j)
-      distances[0 + j * lhs.size()] = j;
+    for (size_t j = 0; j <= rhs.size(); ++j)
+      distances[0 + j * line] = j;
 
-    for (size_t i = 1; i < lhs.size(); ++i)
-      for (size_t j = 1; j < rhs.size(); ++j)
+    // In wikipedia article strings are 1 indexed
+    // In C++ we are 0 indexed
+    for (size_t i = 1; i <= lhs.size(); ++i)
+      for (size_t j = 1; j <= rhs.size(); ++j)
       {
         size_t cost = 0;
-        if (lhs[i] != rhs[j])
+        if (lhs[i - 1] != rhs[j - 1])
           cost = 1;
 
-        distances[i + j * lhs.size()]
+        distances[i + j * line]
           = std::min(
               std::min(
                 // deletion
-                distances[(i - 1) + j * lhs.size()] + 1
+                distances[(i - 1) + j * line] + 1
                 // insertion
-                , distances[i + (j - 1) * lhs.size()] + 1
+                , distances[i + (j - 1) * line] + 1
               )
               // substitution
-              , distances[(i - 1) + (j - 1) * lhs.size()] + cost
+              , distances[(i - 1) + (j - 1) * line] + cost
             );
 
         // transposition
         if (i > 1
             && j > 1
-            && lhs[i] == rhs[j - 1]
-            && lhs[i - 1] == rhs[j])
+            && lhs[i - 1] == rhs[j - 2]
+            && lhs[i - 2] == rhs[j - 1])
         {
-          distances[i + j * lhs.size()]
+          distances[i + j * line]
             = std::min(
-                distances[i + j * lhs.size()]
-                , distances[(i - 2) + (j - 2) * lhs.size()] + cost
+                distances[i + j * line]
+                , distances[(i - 2) + (j - 2) * line] + cost
               );
         }
       }
 
-    // 0 indexed
-    return distances[(lhs.size() * rhs.size() - 1)];
+    return distances.back();
   }
 } /* sqldiff */
