d24b6cdf30f682cdabad2dda1dec253a3c464806
Armand Leclercq <armand@leclercq.site>
2017-08-19 17:03:11 +0000

    Test a single script that creates several tables

--- test/grammar.cc
+++ test/grammar.cc
@@ -65,3 +65,25 @@ TEST(Grammar, Create_Table)
   );
   ASSERT_STREQ("Tata", astCamel.tables[0].name.c_str());
 }
+
+TEST(Grammar, Create_Tables)
+{
+  std::string script = "CREATE TABLE toto (id int); CREATE TABLE Tata (id INT);";
+  sqldiff::sql_grammar<std::string::const_iterator> sql;
+  sqldiff::SQL ast;
+
+  std::string::const_iterator first = std::begin(script);
+  std::string::const_iterator last = std::end(script);
+
+  ASSERT_TRUE(
+    phrase_parse(
+      first
+      , last
+      , sql
+      , boost::spirit::ascii::space
+      , ast
+    )
+  );
+  ASSERT_STREQ("toto", ast.tables[0].name.c_str());
+  ASSERT_STREQ("Tata", ast.tables[1].name.c_str());
+}
